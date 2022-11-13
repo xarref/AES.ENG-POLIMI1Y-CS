c161f87cea842ecb8a1f06882d64edd1bd2b1eb5
xarref <80639290+xarref@users.noreply.github.com>
2022-11-13 18:59:58 +0100

    Update pari_dispari.c
    sistemato i commenti

--- basics/pari_dispari.c
+++ basics/pari_dispari.c
@@ -6,22 +6,22 @@
 #include <stdio.h>  //standard lib
 
 int main(){
-int x;											 //inizializzo la variabile x
+int x;						//inizializzo la variabile x
 
-printf("inserisci un numero: \n");				//chiedo un numero all utente
-scanf("%d", &x);								//scannerizzo il numero 
+printf("inserisci un numero: \n");		//chiedo un numero all utente
+scanf("%d", &x);				//scannerizzo il numero 
 
 printf("%d e' un numero ", x);
 
 
 
-if (x%2 == 0)									//se il numero diviso due fa zero allora e' pari
+if (x%2 == 0)					//se il numero diviso due fa zero allora e' pari
 {
 	printf("pari\n");
 }
 
 else {
-	printf("dispari\n");						//altrimenti e' dispari
+	printf("dispari\n");			//altrimenti e' dispari
 }
 
 	return 0;
