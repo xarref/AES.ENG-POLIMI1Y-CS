2e9949f3fd2b07cecc1238d2fa056e31f6a9316d
xarref <80639290+xarref@users.noreply.github.com>
2022-11-29 13:09:19 +0100

    Add files via upload

---
+++ basics/potenze_di_numeri.c
@@ -0,0 +1,38 @@
+/*
+
+Scrivere un programma che, dati in ingresso due numeri interi positivi N
+e K, stampa potenze di N con esponenti da 1 a K.
+Esempio:
+Base: 4
+Esponente massimo: 4
+Potenze: 4 16 64 256
+
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char const *argv[]){
+
+int N,K;
+
+printf("inserisci un numero di cui fare tutte le potenze e fino a che potenza vuoi arrivare:\n");
+scanf("%d %d", &N, &K);
+printf("potenze:\n");
+
+for (int i = 1; i <= K; ++i)
+{
+	int a;
+	a = pow(N,i);
+
+	printf("%d \n", a);
+}
+
+
+
+
+
+
+return 0;
+
+}
